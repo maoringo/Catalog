3a17536d572bf8b5788761f1f721dfeacad20966
Maori Ito <maori@nibio.go.jp>
2013-10-30 12:37:00 +0900

    sample reading file

---
+++ sample.rb
@@ -0,0 +1,35 @@
+require 'roo'
+require 'spreadsheet'
+require 'kconv'
+
+require './excel_base'
+include ExcelBase
+
+lineAry = []
+lineArys = []
+spst = Roo::Excel.new(ARGV[0])
+spst.default_sheet = spst.sheets.first
+1.upto(spst.last_row) do |line|
+for i in 1..17
+lineArys << spst.cell(line,i).to_s
+end
+lineAry << lineArys
+lineArys = []
+end
+
+ 
+default_format = NormalFormat.new
+workbook = Spreadsheet::Workbook.new
+workbook.default_format = default_format
+worksheet = workbook.create_worksheet
+
+lineAry.each_with_index do |line,i|
+    line.each_with_index do |elem,j|
+        worksheet[i,j] = elem
+            if elem.include?("データベース")
+            worksheet.row(i).set_format(j,ColorFormat.new(:orange))
+            end
+    end
+end
+
+workbook.write('sample2.xls')
